4fb198bbb3c7ce3da0eca08789fd24a234b98f04
MilanTam <milantamburic@hotmail.com>
2020-05-04 18:36:32 +0200

    init

---
+++ src/at/MT/Dslr.java
@@ -0,0 +1,55 @@
+package at.MT;
+
+import java.util.Scanner;
+
+public class Dslr {
+    private String brand = "";
+    private String model = "";
+    private Lens lensInUse = null;
+    private MemoryCard memoryCardInUse = null;
+    Scanner userInterface = new Scanner(System.in);
+
+    public Dslr(String brand, String model, int year) {
+    }
+
+    public void setLens(Lens lensInUse) {
+        this.lensInUse = lensInUse;
+        System.out.println("I switched my lens to " + getLensInUse());
+    }
+
+    public String getLensInUse() {
+        return lensInUse.toString();
+    }
+
+    public void setMemoryCard(MemoryCard memoryCardInUse) {
+        this.memoryCardInUse = memoryCardInUse;
+        System.out.println("I switched my memorycard to " + getMemoryCardInUse());
+    }
+
+    public String getMemoryCardInUse() {
+        return memoryCardInUse.toString();
+    }
+
+    public void makePhoto(int amountOfRepetitions) {
+        for (int i = 0; i < amountOfRepetitions; i++) {
+            System.out.println("Click!");
+            memoryCardInUse.savePhoto();
+            if ((memoryCardInUse.getRemainingPhotos() < 3.0) || (memoryCardInUse.getFreeCapacityInPercent() < 5.0)) {
+                memoryCardInUse.deletePhotos();
+            }
+        }
+    }
+
+    public void checkMemory() {
+        System.out.println(getMemoryCardInUse() + " Status: " + memoryCardInUse.getFreeCapacityInPercent() + "% unused Memory left");
+    }
+
+    public void checkRemainingPhotos() {
+        System.out.println("Remaining Memory on " + getMemoryCardInUse() + " lasts for " + memoryCardInUse.getRemainingPhotos() + " photos");
+    }
+
+    public String toString() {
+        return brand + model;
+    }
+
+}
---
+++ src/at/MT/Main.java
@@ -0,0 +1,78 @@
+package at.MT;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Aufgabe CAM
+
+        Camera sony = new Camera();
+
+        sony.setBrand("Sony");
+        sony.setModel("Cyber shot™ DSC-RX100");
+        sony.setYear(2020);
+        sony.setResolution(20);
+        sony.setFocalLength(28,100);
+        sony.setMadeIn("Japan");
+        sony.setPrice(299.90);
+
+        sony.getSpecsheet();
+
+
+        Camera canon = new Camera();
+
+        canon.setBrand("Canon");
+        canon.setModel("Ixus 185");
+        canon.setYear(2018);
+        canon.setResolution(19);
+        canon.setFocalLength(5,40);
+        canon.setMadeIn("Japan");
+        canon.setPrice(99.99);
+
+        canon.getSpecsheet();
+
+
+        Camera panasonic = new Camera();
+
+        panasonic.setBrand("Panasonic");
+        panasonic.setModel("DC-TZ202EG-K");
+        panasonic.setYear(2019);
+        panasonic.setResolution(20);
+        panasonic.setFocalLength(8,132);
+        panasonic.setMadeIn("Japan");
+        panasonic.setPrice(619.00);
+
+        panasonic.getSpecsheet();
+
+
+        //Aufgabe Fotoapparat1
+
+        Dslr myCanon = new Dslr("Canon","5d Mark IV", 2016);
+
+        Lens standardKitLens = new Lens("Canon","Wide Angle",18,35);
+
+        Lens telephotoLens = new Lens("Canon","Telephoto", 70,200);
+
+        MemoryCard privateMemoryCard = new MemoryCard("Medion",32);
+        MemoryCard businessMemoryCard = new MemoryCard("Sony",1024);
+
+        myCanon.setLens(telephotoLens);
+        myCanon.setMemoryCard(privateMemoryCard);
+
+        myCanon.makePhoto(7);
+
+        myCanon.setMemoryCard(businessMemoryCard);
+        myCanon.makePhoto(5);
+
+        myCanon.setLens(standardKitLens);
+        myCanon.makePhoto(7);
+
+
+        System.out.println(businessMemoryCard.getFreeCapacityInPercent());
+        System.out.println(businessMemoryCard.getRemainingPhotos());
+
+        System.out.println(privateMemoryCard.getFreeCapacityInPercent());
+        System.out.println(privateMemoryCard.getRemainingPhotos());
+
+    }
+}
